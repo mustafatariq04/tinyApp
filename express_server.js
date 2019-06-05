efffa404861572ec69c70efa43e65843509fb122
Mustafa Tariq <mustafatariq04@gmail.com>
2019-06-05 16:45:49 +0000

    updated delete button to remove url from main page

--- express_server.js
+++ express_server.js
@@ -47,6 +47,11 @@ app.post("/urls", (req, res) => {
 });
 
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
